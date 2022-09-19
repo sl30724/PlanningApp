ba48a5dc0758cafd76e639addb13e927c65206a8
Sandy Lee <sl30724@gmail.com>
2022-09-19 11:39:02 -0600

    Initial Commit

---
+++ Planning/PlanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanningApp.swift
+//  Planning
+//
+//  Created by Sandy Lee on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PlanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
